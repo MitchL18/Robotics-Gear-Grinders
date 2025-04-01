f437f1dbfc1420df5609e1abce6f8215828ca576
jschneider17 <jack.schneider1@marist.edu>
2025-04-01 18:48:08 -0400

    Update wallFollowingLab.ino

--- wallFollowingLab/wallFollowingLab.ino
+++ wallFollowingLab/wallFollowingLab.ino
@@ -30,7 +30,20 @@ void setup() {
 
   // may have to apply limits on Ki to prevent integral windup
 
-  
+  // derivative is difference between error and the priorError
+  float derivative = kd * (error - previousError);
+  // set previous error for next round
+  previousError = error;
+
+  // Are previousError and priorError the same thing?
+
+  // sum P. I. and D. together
+  float pidResult = proportional + integral + derivative;
+
+  // apply the pid result to the motors
+  // one wheel will be + pidResult, the other will be - pidResult
+  // positive or negative pidResult value will determine which
+  // direction the robot will turn
 }
 
 void loop() {
